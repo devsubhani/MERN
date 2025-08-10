55b2ea9d1c394c8da88bc1bb1921dff4f1638000
Farrukh Shahzad Subhani <farrukh@renesistech.com>
2025-08-10 15:59:18 +0500

    Add mongoose as a dependency and establish MongoDB connection in the Express server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/MERNDB")
+.then(() => {
+  console.log("Connected to MongoDB");
+})
+.catch((err) => {
+  console.log(err);
+});
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
